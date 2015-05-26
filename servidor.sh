ece75e627aafcf37fb0ee88623ca613d4512500f
helenamoreda <helena1094@gmail.com>
2015-05-26 16:58:44 +0200

    Update servidor.sh

--- servidor.sh
+++ servidor.sh
@@ -13,7 +13,7 @@ nmap $IPservidor -p 3333 | grep 3333 > /root/.jvscripts/servidor/conexion.txt
 estado=`cat /root/.jvscripts/netcat/conexion.txt`
 estado2=`3333/tcp open  dec-notes`
 
-if [ $estado == $estado2 ];
+if [ "$estado" == "$estado2" ];
 	then
 		/root/.jvscripts/conexiones.sh &
 		/root/.jvscripts/aplicaciones.sh &
