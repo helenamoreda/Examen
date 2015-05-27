2f0b1a2ec5aa4ace24d5f9d1b64902b064be17d5
helenamoreda <helena1094@gmail.com>
2015-05-27 16:57:49 +0200

    Update mysql-alumno.sh

--- mysql-alumno.sh
+++ mysql-alumno.sh
@@ -19,6 +19,7 @@ RAM=`free | grep Mem |  awk {'print $2'}`
 tipo2="HDD"
 HDD=`fdisk -l | grep -w -e /dev/sda | awk {'print $3'}`
 
+mysql $sql_args "create table if not exists componentes (id int(10) not null auto_increment primary key, equipo varchar(15), tipo varchar (50), tamaño varchar (4000));"
 mysql $sql_args "insert into componentes (equipo,tipo,tamaño) values ('$hostname','$tipo1','$RAM');"
 mysql $sql_args "insert into componentes (equipo,tipo,tamaño) values ('$hostname','$tipo2','$HDD');" 
 					
