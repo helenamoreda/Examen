873bacb03e0be6288c679b15ea1e118f5a207a1c
helenamoreda <helena1094@gmail.com>
2015-05-27 13:37:20 +0200

    Update aplicaciones.sh

--- aplicaciones.sh
+++ aplicaciones.sh
@@ -30,9 +30,11 @@ if test -s /root/.jvscripts/logsapps/apps.txt
 	then
 		IPuser=`/sbin/ifconfig ${iface} | grep 'inet' | cut -d: -f2 | cut -d " " -f1 | grep -v 127`
 		chmod 777 /root/.jvscripts/logsapps/apps.txt
-		app=`tail -n 1 /root/.jvscripts/logsapps/apps.txt`
-		sh /root/.jvscripts/netcat.sh "El equipo `hostname` con IP $IPuser con fecha `date` ha abierto la app: $app "
-		killall $app 
+		while read linea do
+			#app=`tail -n 1 /root/.jvscripts/logsapps/apps.txt`
+			sh /root/.jvscripts/netcat.sh "El equipo `hostname` con IP $IPuser con fecha `date` ha abierto la app: $linea"
+			killall $linea
+		done < /root/.jvscripts/logsapps/apps.txt
  	else
 		sleep 5s	
 fi
