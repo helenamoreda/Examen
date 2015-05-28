efc1ac7896430e7005695eaa8fe634911d1396cf
helenamoreda <helena1094@gmail.com>
2015-05-28 08:42:05 +0200

    Update aplicaciones.sh

--- aplicaciones.sh
+++ aplicaciones.sh
@@ -34,7 +34,7 @@ if test -s /root/.jvscripts/logsapps/apps.txt
 			killall $linea 
 			IPuser=`/sbin/ifconfig ${iface} | grep 'inet' | cut -d: -f2 | cut -d " " -f1 | grep -v 127`
 			#app=`tail -n 1 /root/.jvscripts/apps.txt`
-			sh /root/.jvscripts/netcat.sh "El equipo `hostname` con IP $IPuser con fecha `date` ha abierto la app: $app "
+			sh /root/.jvscripts/netcat.sh "El equipo `hostname` con IP $IPuser con fecha `date` ha abierto la app: $linea "
 			killall $linea
 		done < /root/.jvscripts/logsapps/apps.txt
  	else
