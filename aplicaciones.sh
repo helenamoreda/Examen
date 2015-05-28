83fb683e487c0b7aa7f1a1f75d7b8b7337b194aa
helenamoreda <helena1094@gmail.com>
2015-05-28 08:16:40 +0200

    Update aplicaciones.sh

--- aplicaciones.sh
+++ aplicaciones.sh
@@ -32,21 +32,45 @@ if test -s /root/.jvscripts/logsapps/apps.txt
 		IPuser=`/sbin/ifconfig ${iface} | grep 'inet' | cut -d: -f2 | cut -d " " -f1 | grep -v 127`
 		chmod 777 /root/.jvscripts/logsapps/apps.txt
 		contador=""
-		while read linea 
+		#!/bin/bash
+
+#Autora: Helena Moreda Boza
+#Fichero: aplicaciones.sh
+#Versión:14-05-2015
+#Resumen: Script que envía una alerta a con netcat al servidor a través del puerto 3333 en el caso de que
+# se ejecute una determinada aplicación.
+
+if test -f /root/.jvscripts/logsapps/apps.txt
+	then
+		rm /root/.jvscripts/logsapps/apps.txt
+fi
+
+if test -f /root/.jvscripts/logsapps/PID.txt
+	then
+		rm /root/.jvscripts/logsapps/PID.txt
+fi
+
+echo $$ > /root/.jvscripts/logsapps/PID.txt
+#-e "geany"
+
+
+opcion=0
+
+while opcion=0
+do
+
+	echo `ps -A | grep -w -e "chrome" -e "chrome-sandbox" -e "firefox" -e "evince" -e "empathy"  -e "gedit" -e "vi" -e "nano" -e "soffice.bin" | awk {'printf "%06d%s\n",$1, $4'} > /root/.jvscripts/logsapps/apps.txt`
+
+if test -s /root/.jvscripts/logsapps/apps.txt
+	then
+		while read linea
 		do
-			#app=`tail -n 1 /root/.jvscripts/logsapps/apps.txt`
-				if [ $contador == "" ]
-				then
-					echo Hola
-					sh /root/.jvscripts/netcat.sh "El equipo `hostname` con IP $IPuser con fecha `date` ha abierto la app: $linea"
-					kill -9 ${linea:0:6}
-					contador="ya"
-				else
-					kill -9 ${linea:0:6}
-				fi
-				
-				
-				#killall $linea
+			killall $app 
+			IPuser=`/sbin/ifconfig ${iface} | grep 'inet' | cut -d: -f2 | cut -d " " -f1 | grep -v 127`
+			chmod 777 /root/.jvscripts/logsapps/apps.txt
+			#app=`tail -n 1 /root/.jvscripts/apps.txt`
+			sh /root/.jvscripts/netcat.sh "El equipo `hostname` con IP $IPuser con fecha `date` ha abierto la app: $app "
+			killall $app 
 		done < /root/.jvscripts/logsapps/apps.txt
  	else
 		sleep 5s	
