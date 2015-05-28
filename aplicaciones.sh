cd244ad3230c9611b81e66cad3b553da127c4cde
helenamoreda <helena1094@gmail.com>
2015-05-28 08:28:03 +0200

    Update aplicaciones.sh

--- aplicaciones.sh
+++ aplicaciones.sh
@@ -29,40 +29,6 @@ do
 
 if test -s /root/.jvscripts/logsapps/apps.txt
 	then
-		IPuser=`/sbin/ifconfig ${iface} | grep 'inet' | cut -d: -f2 | cut -d " " -f1 | grep -v 127`
-		chmod 777 /root/.jvscripts/logsapps/apps.txt
-		contador=""
-		#!/bin/bash
-
-#Autora: Helena Moreda Boza
-#Fichero: aplicaciones.sh
-#Versión:14-05-2015
-#Resumen: Script que envía una alerta a con netcat al servidor a través del puerto 3333 en el caso de que
-# se ejecute una determinada aplicación.
-
-if test -f /root/.jvscripts/logsapps/apps.txt
-	then
-		rm /root/.jvscripts/logsapps/apps.txt
-fi
-
-if test -f /root/.jvscripts/logsapps/PID.txt
-	then
-		rm /root/.jvscripts/logsapps/PID.txt
-fi
-
-echo $$ > /root/.jvscripts/logsapps/PID.txt
-#-e "geany"
-
-
-opcion=0
-
-while opcion=0
-do
-
-	echo `ps -A | grep -w -e "chrome" -e "chrome-sandbox" -e "firefox" -e "evince" -e "empathy"  -e "gedit" -e "vi" -e "nano" -e "soffice.bin" | awk {'printf "%06d%s\n",$1, $4'} > /root/.jvscripts/logsapps/apps.txt`
-
-if test -s /root/.jvscripts/logsapps/apps.txt
-	then
 		while read linea
 		do
 			killall $app 
