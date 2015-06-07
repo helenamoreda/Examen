7365d563591cfd186e58411f2ccb83c2d1529d59
helenamoreda <helena1094@gmail.com>
2015-06-07 20:42:50 +0200

    Update mysql-alumno2.sh

--- mysql-alumno2.sh
+++ mysql-alumno2.sh
@@ -23,9 +23,7 @@ HDD=`fdisk -l | grep -w -e /dev/sda | awk {'print $3'}`
 mysql $sql_args "insert into componentes2 (equipo,tipo,tamaño) values ('$hostname','$tipo1','$RAM');"
 mysql $sql_args "insert into componentes2 (equipo,tipo,tamaño) values ('$hostname','$tipo2','$HDD');"
 
-opcion=0
-while [ $opcion -eq 0 ];
-do
+
 mysql $sql_args "select tipo from componentes as t1 where not exists(select equipo, tipo, tamaño from componentes2 as t2 where t1.equipo=t2.equipo and t1.tipo=t2.tipo and t1.tamaño=t2.tamaño);" | tail -1 > /tmp/tipocambiado
 mysql $sql_args "select tamaño from componentes as t1 where not exists(select equipo, tipo, tamaño from componentes2 as t2 where t1.equipo=t2.equipo and t1.tipo=t2.tipo and t1.tamaño=t2.tamaño);" | tail -1 > /tmp/tamaño1
 mysql $sql_args "select tamaño from componentes2 as t1 where not exists(select equipo, tipo, tamaño from componentes as t2 where t1.equipo=t2.equipo and t1.tipo=t2.tipo and t1.tamaño=t2.tamaño);" | tail -1 > /tmp/tamaño2
@@ -39,5 +37,4 @@ if [ "$tipocambiado" != "" ]
 		sendemail -f cambioshardwarejulioverne@hotmail.com -t helena1094@hotmail.com -s smtp.live.com -u \ "Asunto Cambios en el hardware" -m "Ha habido un cambio en el componente $tipocambiado del equipo $hostname. Su anterior capacidad era $hola y ahora es $hola2" -v -xu cambioshardwarejulioverne@hotmail.com -xp Cambioshardware -o tls=yes
 		opcion=1
 fi
-sleep 5s
-done
+
