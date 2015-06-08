fc2596002107ab28dfec7ee011c8edd84d24b020
helenamoreda <helena1094@gmail.com>
2015-06-08 11:39:30 +0200

    Update comprobacion.sh

--- comprobacion.sh
+++ comprobacion.sh
@@ -7,24 +7,14 @@
 
 hostname=`hostname`
 
-PID1=`cat /root/.jvscripts/logsapps/PID.txt`
+PID1=`cat /root/.jvscripts/mysql/PID.txt`
 echo $PID1
 
-PID2=`ps -a | grep -e aplicaciones.sh | awk {'print $1'}`
+PID2=`ps -a | grep -e mysql-alumno2.sh | awk {'print $1'}`
 echo $PID2
-PID3=`cat /root/.jvscripts/logsconexiones/PID.txt`
 
-PID4=`ps -a | grep -e conexiones.sh | awk {'print $1'}`
 
 if [ "$PID1" != "$PID2" ]
 	then
-		echo no deberia entrar
-		sendemail -f cambioshardwarejulioverne@hotmail.com -t helena1094@hotmail.com -s smtp.live.com -u \ "Script parado" -m "El script aplicaciones.sh ha dejado de funcionar en el equipo $hostname" -v -xu cambioshardwarejulioverne@hotmail.com -xp Cambioshardware -o tls=yes
+		sendemail -f cambioshardwarejulioverne@hotmail.com -t helena1094@hotmail.com -s smtp.live.com -u \ "Script parado" -m "El script mysql-alumno2.sh ha dejado de funcionar en el equipo $hostname" -v -xu cambioshardwarejulioverne@hotmail.com -xp Cambioshardware -o tls=yes
 fi
-
-if [ "$PID3" != "$PID4" ]
-	then
-		echo aqui tampoco
-		sendemail -f cambioshardwarejulioverne@hotmail.com -t helena1094@hotmail.com -s smtp.live.com -u \ "Script parado" -m "El script conexiones.sh ha dejado de funcionar en el equipo $hostname" -v -xu cambioshardwarejulioverne@hotmail.com -xp Cambioshardware -o tls=yes
-fi
-
