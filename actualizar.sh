f1992942dc48a37b5efd9a30804f711e5be479cc
helenamoreda <helena1094@gmail.com>
2015-06-09 20:29:27 +0200

    modificaciones

---
+++ actualizar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Autora: Helena Moreda Boza
+#Fichero: actualizar.sh
+#Versión:09-04-2015
+#Resumen: script que actualiza el repositorio automáticamente
+
+cd /root/.jvscripts/ && git pull https://github.com/helenamoreda/Examen.git
