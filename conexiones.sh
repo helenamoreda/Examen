5d0a79fe9729caff4732525c76311ec731d022ce
helenamoreda <helena1094@gmail.com>
2015-06-09 11:55:22 +0200

    Update conexiones.sh

--- conexiones.sh
+++ conexiones.sh
@@ -33,22 +33,38 @@ opcion=0
 #Entrada del bucle
 while [ $opcion = 0 ] 
 do
+	#Creamos un segundo log que compruebe las conexiones para poder compararlo con el log1
 	echo `netstat -plan | grep "ESTABLECIDO" |grep -e ":80" -e ":443" -e ":21" | awk {'print $5'} | uniq > /root/.jvscripts/logsconexiones/log2.txt`
+	#Contamos sus líneas para poder compararlas con el log1
 	contador2=`wc -l /root/.jvscripts/logsconexiones/log2.txt | cut -d " " -f1`
 
+	
+	#En el caso de que los contadores sean distintos quiere decir que hay una conexión nueva y debe mandar una alerta al servidor
 	if [ "$contador" != "$contador2" ]
 		then
+			#Si el primer contador es menor que el segundo:
 			if [ $contador -lt $contador2 ]
 				then
+					#Almacenamos la nueva conexión en el log1 
 					echo `netstat -plan | grep "ESTABLECIDO" |grep -e ":80" -e ":443" -e ":21" | awk {'print $5'} | uniq > /root/.jvscripts/logsconexiones/log.txt`
+					
+					#Cogemos la ip del usuario
 					IPuser=`/sbin/ifconfig ${iface} | grep 'inet' | cut -d: -f2 | cut -d " " -f1 | grep -v 127`
+				
+					#Almacenamos en la variable “conexion” la última conexión producida en el log para enviarla en la alerta. Tail -n 1 muestra la última línea de un archivo.
 					conexion=`tail -n 1 /root/.jvscripts/logsconexiones/log.txt`
+					
+					#Llamamos al script “netcat.sh” para que se conecte al servidor y envíe nuestra alerta como argumento
 					bash /root/.jvscripts/netcat.sh "El equipo `hostname` con IP $IPuser con fecha `date` ha iniciado una conexión a esta IP $conexion  "
+					
+					#Guardamos todas las conexiones que se produzcan en un log final para poder consultarlo posteriormente
 					echo "El equipo `hostname` con IP $IPuser con fecha `date` ha iniciado una conexión a esta IP $conexion " >>  /root/.jvscripts/logsconexiones/logfinal.txt
 				else
+					#En el caso de que hayan más líneas en $contador quiere decir que se ha cerrado una conexión y por ello mandamos una alerta.
 					bash /root/.jvscripts/netcat.sh "El equipo `hostname` con IP $IPuser con fecha `date` ha cerrado una conexión"
 			fi
 			contador=$contador2
 	fi
+	#En el caso de que no se haya producido ningún cambio en las conexiones espera 5 segundos y vuelve a entrar en el bucle
 	sleep 5s
 done
