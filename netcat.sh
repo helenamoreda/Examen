14ca2eba9858f8bda661fa5ddc9bb1ba6f31f0d9
helenamoreda <helena1094@gmail.com>
2015-06-12 09:43:02 +0200

    Update netcat.sh

--- netcat.sh
+++ netcat.sh
@@ -6,7 +6,7 @@
 #Resumen: script que envía una alerta a través de netcat hacia la ip fija del servidor por el puerto 3333
 
 #Variable que almacena la IP de nuestro servidor. Es importante que esta IP sea fija.
-IPservidor="192.168.1.176"
+IPservidor="172.17.7.20"
 
 #$* imprime todos los argumentos que recibe de los scripts de conexiones y aplicaciones, nc se encarga de establecer conexión con el servidor a través de la IP proporcionada anteriormente y “3333” es el puerto utilizado
 echo $* | nc $IPservidor 3333
