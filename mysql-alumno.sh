f6a1bddcbc2676665d446ecff6ede3cfecdec48d
helenamoreda <helena1094@gmail.com>
2015-06-15 18:44:34 +0200

    Update mysql-alumno.sh

--- mysql-alumno.sh
+++ mysql-alumno.sh
@@ -5,7 +5,7 @@
 #Resumen: Script que se ejecuta con el instalador y envía el hardware actual de la máquina a la base de datos del servidor
 
 #Variable que almacena la IP fija que tiene el servidor
-IPservidor="192.168.176"
+IPservidor="192.168.1.176"
 
 # Se montan los parámetros de conexión
 sql_args="-h $IPservidor -u root -proot hardware -e"
