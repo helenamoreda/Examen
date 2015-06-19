7ad6b7320d5af3f32cdaa53aafdc4ec4f2e2e90b
helenamoreda <helena1094@gmail.com>
2015-06-19 12:35:42 +0200

    Update mysql-alumno2.sh

--- mysql-alumno2.sh
+++ mysql-alumno2.sh
@@ -3,7 +3,7 @@
 #Resumen: Script que se ejecuta al reiniciar la máquina y envía la capacidad del disco duro y RAM periódicamente como un servicio.
 
 #Variable que almacena la IP fija que tiene el servidor
-IPservidor="172.16.171.38"
+IPservidor="192.168.1.177"
 
 # Se montan los parámetros de conexión
 sql_args="-h $IPservidor -u root -proot hardware -e"
